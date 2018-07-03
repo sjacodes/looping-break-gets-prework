e957bcc882502280ccf4d9f5feaf92b33001ad18
Sarah Jacob <smjacob91@gmail.com>
2018-07-03 15:09:22 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,13 @@
 
 def levitation_quiz
-	#your code here
+	
 end
 
 
+describe "#levitation_quiz" do 
+	it "puts out the question 'What is the spell that enacts levitation?' until a user types 'Wingardium Leviosa'. Then, it puts out 'You passed the quiz!'" do 
+		allow(self).to receive(:gets).and_return("Wingardium Leviosa")
+		expect{ levitation_quiz }.to output("What is the spell that enacts levitation?\nYou passed the quiz!\n").to_stdout
+	end
+
+end
